30e140dc20bc4dc5897d0941b2cc398c7d346b36
jason <1074673218@qq.com>
2019-10-14 21:40:16 +0800

    one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
@@ -12,7 +13,8 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title:string = '首页';
+  public title: string = '首页';
+  public flag: boolean = true;
+  public color: string = 'blue';
+  public list: any[] = ['第一项', '第二项', true, 100, new Date()]
+  public binding: any = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+  click(e) {
+    console.log(e)
   }
 
 }
