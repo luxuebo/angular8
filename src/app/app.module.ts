ed6349eec98d4727b370f9ef20c5f0aa16420bbf
jason <jason_lxb@163.com>
2019-10-15 17:55:26 +0800

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 
+//组件
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
 import { HomeComponent } from './components/home/home.component';
-
+//服务
+import {HeroService} from './services/hero.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,7 @@ import { HomeComponent } from './components/home/home.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChildren} from '@angular/core';
+import {HeroService} from '../../services/hero.service'
+//方式一:使用服务
+let service = new HeroService();
 
 @Component({
   selector: 'app-home',
@@ -11,13 +14,27 @@ export class HomeComponent implements OnInit {
   public color: string = 'blue';
   public list: any[] = ['第一项', '第二项', true, 100, new Date()]
   public binding: any = '';
-
-  constructor() { }
-
+  @ViewChildren('vbox') vbox:any;
+  constructor(private heroService:HeroService) {
+   }
   ngOnInit() {
   }
+  ngAfterViewInit(){
+    //当 Angular 初始化完组件视图及其子视图之后调用。第一次 ngAfterContentChecked() 之后调用，只调用一次.
+    //在这个生命周期函数里可以访问dom
+    // console.log('ngAfterViewInit')
+    // console.log(this.vbox.first.nativeElement)
+    // console.log(this)
+  }
+  
   click(e) {
     console.log(e)
   }
+  getService(){
+    //使用服务
+    service.get('方式一:使用服务(不推荐)');
+    this.heroService.get('方式二:使用服务(推荐)');
+  }
+  
 
 }
